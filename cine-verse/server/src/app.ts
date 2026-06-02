0c607be57f7d14b4cd0f1a53a7e12fbd232924eb
Al-Amin Islam <jimalamin7@gmail.com>
2026-06-02 16:12:51 +0600

    fix: make CORS allowedHeaders fully dynamic to accept all client-requested headers

--- cine-verse/server/src/app.ts
+++ cine-verse/server/src/app.ts
@@ -40,14 +40,6 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "cache",
-      "Pragma",
-      "x-requested-with",
-    ],
   }),
 );
 
